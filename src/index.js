e3bba66ff79e9d4413f4dd48dae98ea5395a0d3f
PolianaSc <polianaschuelter@gmail.com>
2022-09-12 12:28:23 +0200

    img weather change

--- src/index.js
+++ src/index.js
@@ -50,6 +50,13 @@ function displayWeather(response) {
   document.querySelector("#day-time").innerHTML = formateDate(
     response.data.dt * 1000
   );
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].descripition);
 }
 
 function searchCity(city) {
