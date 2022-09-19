242d649ac44e1b5e54732f109b9da913fa1b2cea
PolianaSc <polianaschuelter@gmail.com>
2022-09-19 12:26:51 +0200

    function displayForecast without API

--- src/index.js
+++ src/index.js
@@ -26,6 +26,34 @@ function formateDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+          <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+            <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              alt=""
+              width="30"
+            />
+            <div class="weather-forecast-temperature">
+              <span class="weather-forecast-temperature-max"> 25° </span>
+              <span class="weather-forecast-temperature-min"> 12° </span>
+            </div>
+          </div>
+`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeather(response) {
   celsiusTemperature = response.data.main.temp;
   document.querySelector("#temperature").innerHTML =
@@ -125,3 +153,4 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 // Search action
 
 searchCity("Lisbon");
+displayForecast();
