d3e19a5f6c373a9fe3f8662c9ea7a22923c08923
PolianaSc <polianaschuelter@gmail.com>
2022-09-20 12:11:43 +0200

    review code

--- src/index.js
+++ src/index.js
@@ -36,7 +36,6 @@ function formatDayForecast(timestamp) {
 
 function displayForecast(response) {
   let forecast = response.data.daily;
-
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -75,12 +74,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
-
   let apiKey = `445905dadb3d2b0c6f1b916c9d0e3860`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -116,7 +111,6 @@ function displayWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].descripition);
-  console.log(response.data);
   getForecast(response.data.coord);
 }
 
